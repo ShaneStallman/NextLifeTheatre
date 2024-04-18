d2e2075b20fe6d5432411c160ccfde23f3c99fde
Shane-Stallman <shanestallman@gmail.com>
2024-04-18 03:27:11 -0400

    Mobile Optimization Wave 3

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,6 +10,7 @@ const BuildFooter = () => {
             {/* <a href="#home" className="w3-button w3-light-grey"><i className="fa fa-arrow-up w3-margin-right"></i>To the top</a>*/}
               <div className="middle">
                 <a id="subscribe" href="https://forms.gle/3cy2oFT99chJfzCj7" className="newsletter">Join Our Mailing List!</a>
+                <a href={`mailto:${contactInfo.email}`} className="fa fa-envelope icon">&nbsp;{contactInfo.email}</a>
                 <div className="w3-xlarge w3-section socials">
                   <a id="Facebook" href="https://www.facebook.com/profile.php?id=61557075519477" className="fa fa-facebook-official w3-hover-opacity icon"></a>
                   <a id="Instagram" href="https://www.instagram.com/nextlifetheatreco/" className="fa fa-instagram w3-hover-opacity icon"></a>
@@ -22,9 +23,7 @@ const BuildFooter = () => {
                 <p className="selfLink">Developed by <a href="https://github.com/ShaneStallman" title="shane_stallman" target="_blank" className="selfLink"><i className="selfLink fa fa-github icon"></i>Shane Stallman</a></p>
                 
               </div>
-            <div className="contactInfo">
-              <a href={`mailto:${contactInfo.email}`} className="fa fa-envelope icon">&nbsp;{contactInfo.email}</a>
-            </div>
+              
           </footer>
     )
 };
